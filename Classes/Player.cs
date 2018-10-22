9358338e80b39effac1d04b9634235a430c6827f
MiGFiD <migifidi@gmail.com>
2018-10-21 21:15:58 -0400

    First Commit

---
+++ Classes/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    
+    
+    Taken from http://rbwhitaker.wikidot.com/c-sharp-creating-classes
+     
+    The purpose of this program is to learn more about classes.
+
+
+ */
+
+
+
+namespace Classes
+{
+    class Player
+    {
+        private string name;
+        private int score;
+        private int livesLeft;
+
+        public Player(string name, int startingLives)
+        {
+            this.name = name;
+            livesLeft = startingLives;
+        }
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int AddPoints (int totalPoints)
+        {
+            return score += totalPoints;
+        }
+
+        public void Kill()
+        {
+            if (livesLeft > 0)
+            {
+                livesLeft--;
+            }
+        }
+
+        public int GetLivesLeft()
+        {
+            return livesLeft;
+        }
+          
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player player = new Player("Frankenstein");
+
+            while (player.GetLivesLeft() > 0)
+            {
+                player.AddPoints(100);
+
+                player.Kill();
+            }
+
+        }
+    }
+}
